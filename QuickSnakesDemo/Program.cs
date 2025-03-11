aadd52302d56440da0e10efdc78a7b41b10434cf
Anthony Shaw <anthony.p.shaw@gmail.com>
2025-03-11 15:47:02 +1100

    Add project files.

---
+++ QuickSnakesDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using CSnakes.Runtime;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var appFolder = Path.Join(Environment.CurrentDirectory, "python");
+
+var builder = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services
+            .WithPython()
+            .WithHome(appFolder)
+            .WithVirtualEnvironment(Path.Combine(appFolder, ".venv"))
+            .FromRedistributable();
+    });
+var app = builder.Build();
+var env = app.Services.GetRequiredService<IPythonEnvironment>();
+var paddleOcr = env.PaddleOcr();
+paddleOcr.Predict();
---
+++ QuickSnakesDemo/python/paddle_ocr.py
@@ -0,0 +1,20 @@
+﻿from paddleocr import PaddleOCR,draw_ocr
+import os
+import os.path
+
+def predict() -> None:
+    ocr = PaddleOCR()
+    img_path = os.path.join(os.getcwd(), 'python', 'images', '254.jpg')
+    result = ocr.ocr(img_path,rec=False)
+    for idx in range(len(result)):
+        res = result[idx]
+        for line in res:
+            print(line)
+    
+    from PIL import Image
+    result = result[0]
+    image = Image.open(img_path).convert('RGB')
+    im_show = draw_ocr(image, result, txts=None, scores=None, font_path='./simfang.ttf')
+    im_show = Image.fromarray(im_show)
+    im_show.save('./result.jpg')
+    print("Complete")
